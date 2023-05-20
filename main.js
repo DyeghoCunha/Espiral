c38dc24b91e97dba6b278cf1b23e5f77aaece5d2
Dyegho <dyeghocunha@gmail.com>
2023-05-20 14:14:19 -0300

    Final

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const papel = document.getElementById("papel"),
-      caneta = papel.getContext("2d");
+  caneta = papel.getContext("2d");
 
 const obter = seletor => document.getElementById(seletor);
 
@@ -31,7 +31,7 @@ for (let i = 0; i < 21; i++) {
 
 const configuracoes = {
   horaInicio: new Date().getTime(), // Isso pode estar no futuro
-  duracao: 900/3, // Tempo total para todos os pontos se realinharem no ponto de partida
+  duracao: 900 / 3, // Tempo total para todos os pontos se realinharem no ponto de partida
   maxCiclos: Math.max(cores.length, 100), // Deve ser maior que o tamanho de cores, senão...
   somHabilitado: false, // O usuário ainda precisa interagir com a tela primeiro
   pulsoHabilitado: true, // O pulso só será exibido se o som também estiver habilitado
@@ -39,15 +39,15 @@ const configuracoes = {
 }
 
 const obterNomeArquivo = index => {
-  if(configuracoes.instrument === "default") return `key-${index}`; 
-  
+  if (configuracoes.instrument === "default") return `key-${index}`;
+
   return `${configuracoes.instrument}-key-${index}`;
 }
- 
-const obterUrl = indice => `Som/${obterNomeArquivo(indice)}.wav`;
+
+const obterSom = indice => `Som/${obterNomeArquivo(indice)}.wav`;
 
 
-const alternarSom = (habilitado = !configuracoes.somHabilitado) => {  
+const alternarSom = (habilitado = !configuracoes.somHabilitado) => {
   configuracoes.somHabilitado = habilitado;
   comandos.som.dataset.toggled = habilitado;
 }
@@ -58,10 +58,10 @@ papel.onclick = () => alternarSom();
 
 let arcos = [];
 
-const calcularVelocidade = indice => {  
-    const numeroCiclos = configuracoes.maxCiclos - indice,
-          distanciaPorCiclo = 2 * Math.PI;
-  
+const calcularVelocidade = indice => {
+  const numeroCiclos = configuracoes.maxCiclos - indice,
+    distanciaPorCiclo = 2 * Math.PI;
+
   return (numeroCiclos * distanciaPorCiclo) / configuracoes.duracao;
 }
 
@@ -71,15 +71,15 @@ const calcularProximoTempoImpacto = (tempoImpactoAtual, velocidade) => {
 
 const calcularOpacidadeDinamica = (tempoAtual, ultimoTempoImpacto, opacidadeBase, opacidadeMaxima, duracao) => {
   const tempoDesdeImpacto = tempoAtual - ultimoTempoImpacto,
-        porcentagem = Math.min(tempoDesdeImpacto / duracao, 1),
-        deltaOpacidade = opacidadeMaxima - opacidadeBase;
-  
+    porcentagem = Math.min(tempoDesdeImpacto / duracao, 1),
+    deltaOpacidade = opacidadeMaxima - opacidadeBase;
+
   return opacidadeMaxima - (deltaOpacidade * porcentagem);
 }
 
 const determinarOpacidade = (tempoAtual, ultimoTempoImpacto, opacidadeBase, opacidadeMaxima, duracao) => {
-  if(!configuracoes.pulsoHabilitado) return opacidadeBase;
-  
+  if (!configuracoes.pulsoHabilitado) return opacidadeBase;
+
   return calcularOpacidadeDinamica(tempoAtual, ultimoTempoImpacto, opacidadeBase, opacidadeMaxima, duracao);
 }
 
@@ -88,8 +88,8 @@ const calcularPosicaoNoArco = (centro, raio, angulo) => ({
   y: centro.y + raio * Math.sin(angulo)
 });
 
-const tocarTecla = indice => {  
-  const audio = new Audio(obterUrl(indice));
+const tocarTecla = indice => {
+  const audio = new Audio(obterSom(indice));
 
   audio.volume = 0.15;
 
@@ -99,8 +99,8 @@ const tocarTecla = indice => {
 const inicializar = () => {
   arcos = cores.map((cor, indice) => {
     const velocidade = calcularVelocidade(indice),
-          ultimoTempoImpacto = 0,
-          proximoTempoImpacto = calcularProximoTempoImpacto(configuracoes.horaInicio, velocidade);
+      ultimoTempoImpacto = 0,
+      proximoTempoImpacto = calcularProximoTempoImpacto(configuracoes.horaInicio, velocidade);
 
     return {
       cor,
@@ -113,26 +113,26 @@ const inicializar = () => {
 
 const desenharArco = (x, y, raio, inicio, fim, acao = "contorno") => {
   caneta.beginPath();
-  
+
   caneta.arc(x, y, raio, inicio, fim);
-  
-  if(acao === "contorno") caneta.stroke();    
+
+  if (acao === "contorno") caneta.stroke();
   else caneta.fill();
 }
 
 const desenharPontoNoArco = (centro, raioArco, raioPonto, angulo) => {
   const posicao = calcularPosicaoNoArco(centro, raioArco, angulo);
 
-  desenharArco(posicao.x, posicao.y, raioPonto, 0, 2 * Math.PI, "fill");    
+  desenharArco(posicao.x, posicao.y, raioPonto, 0, 2 * Math.PI, "fill");
 }
 
 const desenhar = () => { // Definitivamente não está otimizado
   const tempoAtual = new Date().getTime(),
-        tempoDecorrido = (tempoAtual - configuracoes.horaInicio) / 1000;
-  
+    tempoDecorrido = (tempoAtual - configuracoes.horaInicio) / 1000;
+
   const comprimento = Math.min(window.innerWidth, window.innerHeight) * 0.8,
-        deslocamento = (window.innerWidth - comprimento) / 2;
-  
+    deslocamento = (window.innerWidth - comprimento) / 2;
+
   const inicio = {
     x: deslocamento,
     y: window.innerHeight / 2
@@ -154,6 +154,18 @@ const desenhar = () => { // Definitivamente não está otimizado
     anguloInicio: 0,
     anguloMaximo: 2 * Math.PI
   }
+  const widthPX = window.innerWidth;
+  const heightPX = window.innerHeight;
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const widthPX = window.innerWidth;
+    const heightPX = window.innerHeight;
+  
+    const containerExterior = document.getElementById('containerExterior');
+    containerExterior.style.width = `${widthPX}px`;
+    containerExterior.style.height = `${heightPX}px`;
+
+  });
 
   base.raioInicial = base.comprimento * 0.1;
   base.raioCirculo = base.comprimento * 0.006;
@@ -164,7 +176,7 @@ const desenhar = () => { // Definitivamente não está otimizado
 
   caneta.lineCap = "round";
 
-  arcos.forEach((arco, indice) => { 
+  arcos.forEach((arco, indice) => {
 
 
     const raio = base.raioInicial + (base.espacamento * indice);
@@ -172,22 +184,22 @@ const desenhar = () => { // Definitivamente não está otimizado
     caneta.globalAlpha = determinarOpacidade(tempoAtual, arco.ultimoTempoImpacto, 0.15, 0.65, 1000);
     caneta.lineWidth = base.comprimento * 0.02;
     caneta.strokeStyle = arco.cor;
-    
+
     const deslocamento = base.raioCirculo * (5 / 2) / raio;
-    
+
     desenharArco(centro.x, centro.y, raio, Math.PI + deslocamento, (2 * Math.PI) - deslocamento);
-    
+
     desenharArco(centro.x, centro.y, raio, deslocamento, Math.PI - deslocamento);
   });
-  
+
   arcos.forEach((arco, indice) => { // Desenhar pontos de impacto
     const raio = base.raioInicial + (base.espacamento * indice);
 
     caneta.globalAlpha = determinarOpacidade(tempoAtual, arco.ultimoTempoImpacto, 0.15, 0.85, 1000);
     caneta.fillStyle = arco.cor;
-    
+
     desenharPontoNoArco(centro, raio, base.raioCirculo * 0.75, Math.PI);
-    
+
     desenharPontoNoArco(centro, raio, base.raioCirculo * 0.75, 2 * Math.PI);
   });
 
@@ -197,22 +209,22 @@ const desenhar = () => { // Definitivamente não está otimizado
     caneta.globalAlpha = 1;
     //caneta.fillStyle = arco.cor;
     caneta.fillStyle = cores[1];
-    
-    if(tempoAtual >= arco.proximoTempoImpacto) {      
-      if(configuracoes.somHabilitado) {
+
+    if (tempoAtual >= arco.proximoTempoImpacto) {
+      if (configuracoes.somHabilitado) {
         tocarTecla(indice);
         arco.ultimoTempoImpacto = arco.proximoTempoImpacto;
       }
-      
-      arco.proximoTempoImpacto = calcularProximoTempoImpacto(arco.proximoTempoImpacto, arco.velocidade);      
+
+      arco.proximoTempoImpacto = calcularProximoTempoImpacto(arco.proximoTempoImpacto, arco.velocidade);
     }
-    
+
     const distancia = tempoDecorrido >= 0 ? (tempoDecorrido * arco.velocidade) : 0,
-          angulo = (Math.PI + distancia) % base.anguloMaximo;
-    
+      angulo = (Math.PI + distancia) % base.anguloMaximo;
+
     desenharPontoNoArco(centro, raio, base.raioCirculo, angulo);
   });
-  
+
   requestAnimationFrame(desenhar);
 }
 
